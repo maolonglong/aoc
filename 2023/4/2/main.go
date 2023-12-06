161cb9e5a9361a87c01d7ba0580489700f014e4d
Shaolong Chen <shaolong.chen@outlook.it>
2023-12-06 18:17:34 +0800

    day4

---
+++ 2023/4/2/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	mapset "github.com/deckarep/golang-set/v2"
+	"github.com/samber/lo"
+)
+
+func main() {
+	f := lo.Must(os.Open("./input"))
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	st := mapset.NewThreadUnsafeSet[int]()
+	var sum int
+	m := make(map[int]int)
+	for {
+		st.Clear()
+
+		line, err := r.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		l := parseLine(line)
+		for _, x := range l.Win {
+			st.Add(x)
+		}
+		var score int
+		for _, x := range l.Have {
+			if st.Contains(x) {
+				score++
+			}
+		}
+		n := m[l.Card] + 1
+		sum += score*n + 1
+		for i := l.Card + 1; i <= l.Card+score; i++ {
+			m[i] += n
+		}
+	}
+
+	fmt.Println(sum)
+}
+
+type Line struct {
+	Win  []int
+	Have []int
+	Card int
+}
+
+var _re = regexp.MustCompile(`Card\s+(\d+):\s+(.*?)\|(.*)`)
+
+func parseLine(line []byte) Line {
+	matched := _re.FindSubmatch(line)
+	var l Line
+	l.Card, _ = strconv.Atoi(string(matched[1]))
+
+	for _, x := range strings.Fields(string(matched[2])) {
+		num, _ := strconv.Atoi(x)
+		l.Win = append(l.Win, num)
+	}
+
+	for _, x := range strings.Fields(string(matched[3])) {
+		num, _ := strconv.Atoi(x)
+		l.Have = append(l.Have, num)
+	}
+
+	return l
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module github.com/maolonglong/aoc
 
 go 1.21.3
 
-require github.com/samber/lo v1.38.1
+require (
+	github.com/deckarep/golang-set/v2 v2.5.0
+	github.com/samber/lo v1.38.1
+)
 
 require golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
