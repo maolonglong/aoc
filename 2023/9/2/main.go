599a68d4551d9eae2506a194d3e1bb40f277db7c
Shaolong Chen <shaolong.chen@outlook.it>
2023-12-09 19:44:25 +0800

    day9

---
+++ 2023/9/2/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+func main() {
+	f := lo.Must(os.Open("./input"))
+	defer f.Close()
+
+	var sum int
+
+	r := bufio.NewReader(f)
+	for {
+		line, err := r.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		items := strings.Fields(line)
+		var seq [][]int
+		var n int
+		seq = append(seq, make([]int, 0, len(items)))
+		for _, it := range items {
+			x, _ := strconv.Atoi(it)
+			seq[0] = append(seq[0], x)
+		}
+
+		for {
+			seq = append(seq, make([]int, len(seq[n])-1))
+			for i := 0; i+1 < len(seq[n]); i++ {
+				seq[n+1][i] = seq[n][i+1] - seq[n][i]
+			}
+			n++
+
+			if lo.EveryBy(seq[n], func(item int) bool { return item == 0 }) {
+				break
+			}
+		}
+
+		var start int // 0
+		for i := n - 1; i >= 0; i-- {
+			start = seq[i][0] - start
+		}
+		sum += start
+	}
+
+	fmt.Println(sum)
+}
